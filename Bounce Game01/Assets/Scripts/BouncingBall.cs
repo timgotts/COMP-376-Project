1e2468ed9da1bb6438d7c645448915266eeedd77
TarikAzz <tarikazzouni@hotmail.com>
2017-10-28 18:31:41 -0400

    Added Animations and Sound
    Added sprites and made the blob bounce animation. Also added level
    music, and bounce sound.
    
    BouncingBall.cs:
    --Added an Animator, and animations to make the blob bounce, along with
    a hasBounced bool variable.

--- Bounce Game01/Assets/Scripts/BouncingBall.cs
+++ Bounce Game01/Assets/Scripts/BouncingBall.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class BouncingBall : MonoBehaviour {
+public class BouncingBall : MonoBehaviour
+{
 
     public string key01;
     public string key02;
@@ -12,19 +13,34 @@ public class BouncingBall : MonoBehaviour {
     public float transformForce = 3;
     public float JumpSpeed = 200;
     private Rigidbody2D rigid;
+
+    private Animator animator;
+    bool hasBounced;
+    public AudioClip bounceSound;
+    private AudioSource audioSource;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
+       
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetAxis(key01) == 1)
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set the Animator's bool variable.
+        animator.SetBool("hasBounced", hasBounced);
+
+        if (Input.GetAxis(key01) == 1)
         {
             transform.Translate(Vector3.right * Time.deltaTime * speed);
             rigid.AddForce(Vector3.right * transformForce, ForceMode2D.Force);
+
         }
         if (Input.GetAxis(key02) == 1)
         {
@@ -35,7 +51,7 @@ public class BouncingBall : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid.bodyType = RigidbodyType2D.Dynamic;
-            rigid.velocity = new Vector2(0,0);
+            rigid.velocity = new Vector2(0, 0);
             this.transform.parent = null;
         }
         if (Input.GetKeyDown(KeyCode.J))
@@ -46,6 +62,9 @@ public class BouncingBall : MonoBehaviour {
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        audioSource.PlayOneShot(bounceSound);
+        hasBounced = true;
+
         if (collision.gameObject.tag == "ColliderType1")
         {
             rigid.AddForce(Vector3.up * force, ForceMode2D.Impulse);
@@ -53,21 +72,20 @@ public class BouncingBall : MonoBehaviour {
         else if (collision.gameObject.tag == "ColliderType2")
         {
             //rigid.AddForceAtPosition(Vector3.up * force * 15.0f, transform.position, ForceMode2D.Impulse);
-           // Vector3 reflect = Vector3.Reflect(transform.position, Vector3.right);
-            rigid.AddForce(Vector3.up * force , ForceMode2D.Impulse);
+            // Vector3 reflect = Vector3.Reflect(transform.position, Vector3.right);
+            rigid.AddForce(Vector3.up * force, ForceMode2D.Impulse);
             //rigid.AddForce(reflect * Time.deltaTime * 5, ForceMode2D.Impulse);
         }
 
         else if (collision.gameObject.GetComponent<YelloType>())
         {
-            
             //rigid.AddForceAtPosition(Vector3.up * force, transform.position, ForceMode2D.Impulse);
             collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
             if (collision.gameObject.GetComponent<YelloType>().isCollided)
             {
                 rigid.AddForce(Vector3.up * force, ForceMode2D.Impulse);
             }
-                 
+
         }
         else if (collision.gameObject.tag == "LooseBorder" || collision.gameObject.tag == "Bomb")
         {
@@ -79,10 +97,14 @@ public class BouncingBall : MonoBehaviour {
             //Debug.Log("Hook");
             rigid.bodyType = RigidbodyType2D.Kinematic;
             this.transform.parent = collision.gameObject.transform;
-
         }
-
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        hasBounced = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "LeftRotateCollider")
